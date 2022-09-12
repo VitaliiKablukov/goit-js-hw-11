c5bab3df9affcea924e4ebf90052eb11374b4fc2
VitaliiKablukov <vetal.kablukov98@gmail.com>
2022-09-12 19:44:26 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,38 +1,77 @@
 import Notiflix from 'notiflix';
+import renderHtmlItems from './js/renderHTML';
+//
 const axios = require('axios');
 const form = document.querySelector('#search-form');
+const { renderHtml, gallery } = renderHtmlItems;
+const morePicturesBtn = document.querySelector('.load-more');
 
+//
 form.addEventListener('submit', getPictures);
+//
+morePicturesBtn.addEventListener('click', anyPictures);
+//
+let userInput;
+let page = 1;
+let perPage = 40;
+//
+
+//
+
 async function getPictures(e) {
-  e.preventDefault();
-  const userInput = e.currentTarget.searchQuery.value;
-  let pageUrl = [];
-  await axios
-    .get(
-      `https://pixabay.com/api/?key=29826556-a4f91074fca654992db1f732d&q=${userInput}&image_type=photo&orientation=horizontal&safesearch=true`
-    )
-    .then(function (response) {
-      Notiflix.Notify.success(`Запит:${userInput} виконаний успішно`);
-      response.data.hits.map(pirtueUrl => pageUrl.push(pirtueUrl.pageURL));
-    })
-    .catch(function (error) {
-      Notiflix.Notify.failure('oops');
-      console.log(error);
-    });
-  console.log(pageUrl);
-  for (let pictures of pageUrl) {
-    console.log(pictures);
-    axios
-      .get(
-        `https://pixabay.com/api/?key=29826556-a4f91074fca654992db1f732d&q=${pictures}`
-      )
-      .then(function (response) {
-        Notiflix.Notify.success(`Запит виконаний успішно`);
-        console.log(response);
-      })
-      .catch(function (error) {
-        Notiflix.Notify.failure('oops');
-        console.log(error);
-      });
+  try {
+    e.preventDefault();
+    gallery.innerHTML = '';
+    userInput = e.currentTarget.searchQuery.value;
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=29826556-a4f91074fca654992db1f732d&q=${userInput}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page}`
+    );
+    if (response.data.totalHits === 0) {
+      Notiflix.Notify.failure(
+        `"Sorry, there are no images matching your search query. Please try again."`
+      );
+    } else {
+      Notiflix.Notify.success(
+        `Hooray! We found ${response.data.totalHits} images.`
+      );
+
+      renderHtml(response);
+      morePicturesBtn.classList.remove(`is-hidden`);
+    }
+  } catch (error) {
+    Notiflix.Notify.failure('oops');
+    console.log(error);
   }
+}
+//
+async function anyPictures() {
+  try {
+    page += 1;
+    morePicturesBtn.classList.add(`is-hidden`);
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=29826556-a4f91074fca654992db1f732d&q=${userInput}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page}`
+    );
+    if (perPage * page > response.data.totalHits) {
+      Notiflix.Notify.success(
+        "We're sorry, but you've reached the end of search results."
+      );
+    } else {
+      renderHtml(response);
+      morePicturesBtn.classList.remove(`is-hidden`);
+      scroll();
+    }
+  } catch (error) {
+    Notiflix.Notify.failure('oops');
+    console.log(error);
+  }
+}
+function scroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 6,
+    behavior: 'smooth',
+  });
 }
---
+++ src/js/renderHTML.js
@@ -0,0 +1,35 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const gallery = document.querySelector('.gallery');
+let pageUrl = [];
+function renderHtml(data) {
+  data.data.hits.map(pirtueUrl => {
+    const renderHtml = `<div class="photo-card">
+  <a class="gallery__item" href="${pirtueUrl.largeImageURL}"><img  src=${pirtueUrl.webformatURL} alt="${pirtueUrl.tags}" loading="lazy" /></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes<br>${pirtueUrl.likes}</b>
+    </p>
+    <p class="info-item"> 
+      <b>Views<br>${pirtueUrl.views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments<br>${pirtueUrl.comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads<br>${pirtueUrl.downloads}</b>
+    </p>
+  </div>
+</div>`;
+    pageUrl.push(renderHtml);
+  });
+  gallery.innerHTML = pageUrl.join('');
+  const lightBoxGallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+  0;
+  lightBoxGallery.refresh();
+}
+
+export default { renderHtml, gallery };
